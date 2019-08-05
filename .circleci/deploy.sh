0787ebdfdd9c7dfc057486156cd5aa32be1e7515
sobaya <sobaya007@gmail.com>
2019-08-06 08:28:48 +0900

    Fix: CircleCI

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -3,8 +3,17 @@
 set -e
 set -u
 
-git config user.name CircleCI
-git config user.email <>
-git add -A docs
-git commit -m '[skip ci] Update docs'
-git push origin master
+source "$(curl -fsS  --retry 3 https://dlang.org/install.sh | bash -s $1 --activate)"
+dub build --build=ddox
+
+git config --global push.default simple
+git config --global user.name "CircleCI"
+git config --global user.email "<>"
+
+mkdir ~/.ssh && echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+
+git checkout -b gh-pages
+git pull origin gh-pages
+git add -A --force docs
+git commit -m "[skip ci] Update docs"
+git subtree push --prefix docs/ origin gh-pages
